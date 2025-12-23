3585e1101686e41eb17ad8899b82aa1f2e92e03e
Yoshiki Shibata <yoshiki.shibata@gmail.com>
2025-12-24 07:18:40 +0900

    updated

--- src/msgtool/MessagingToolVersion.java
+++ src/msgtool/MessagingToolVersion.java
@@ -1,8 +1,8 @@
 /*
  * File: MessagingToolVersion.java - last edit:
- * Yoshiki Shibata 27-Mar-2014
+ * Yoshiki Shibata 24-Dec-2025
  * 
- * Copyright (c) 2004, 2007, 2014 by Yoshiki Shibata. All rights reserved.
+ * Copyright (c) 2004, 2007, 2014, 2025 by Yoshiki Shibata. All rights reserved.
  */
 package msgtool;
 
@@ -11,10 +11,11 @@ public class MessagingToolVersion {
     private MessagingToolVersion() {
     }
 
-    public static final String VERSION = "2.60";
+    public static final String VERSION = "2.61";
 }
 
 // LOG
 // 2.52 :  5-Sep-04 Y.Shibata moved from AboutUI
 // 2.53 : 24-Nov-07	Y.Shibata I restarted working on this tool again.
 // 2.60 : 23-Mar-14 Y.Shibata I restarted working on this tool again with Java 8
+// 2.61 : 24-Dec-25 Y.Shibata showed IPv4 address
--- src/msgtool/util/NetUtil.java
+++ src/msgtool/util/NetUtil.java
@@ -1,10 +1,11 @@
 // File: NetUtil.java - last edit:
-// Yoshiki Shibata 8-Nov-03
+// Yoshiki Shibata 24-Dec-2025
 
-// Copyright (c) 1998, 2003 by Yoshiki Shibata
+// Copyright (c) 1998, 2003, 2025 by Yoshiki Shibata
 
 package msgtool.util;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -19,12 +20,14 @@ public final class NetUtil {
 	static public String getMyIPAddress() {
 		if (myIPAddress != null)
 			return myIPAddress;
-        
+
 		try {
 			for (Enumeration<NetworkInterface> i =  NetworkInterface.getNetworkInterfaces(); i.hasMoreElements(); ) {
 				NetworkInterface ni = i.nextElement();
 				for (Enumeration<InetAddress> j = ni.getInetAddresses(); j.hasMoreElements();) {
-					InetAddress ia = j.nextElement();	
+					InetAddress ia = j.nextElement();
+					if (!(ia instanceof Inet4Address))
+						continue;
 					if (ia.getHostAddress().equals("127.0.0.1"))
 						continue;
 					myIPAddress = ia.getHostAddress();
@@ -50,3 +53,4 @@ public final class NetUtil {
 // LOG
 // 1.95 : 12-Jul-98 Y.Shibata   created from the old Util.java
 // 2.50 :  8-Nov-03	Y.Shibata	modified for SDK 1.4
+// 2.61 : 24-Dec-25 Y.Shibata	showed IPv4 address
